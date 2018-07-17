4e01aa38beb28e0885070e0d9a09a5200a9570bf
jaridwarren <jaridwarren@gmail.com>
2018-07-16 22:29:24 -0700

    adding styling for stories, story section, and event listener

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-// Built by LucyBot. www.lucybot.com
-
 $(document).ready(function () {
   
   $('select').on('change', function() {
-
+    var selection = $(this).val();
     var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
     url += '?' + $.param({
       'api-key': "eb429d0d01c04f9a8c944a8366666c40"
-  });
-  $.ajax({
-    url: url,
-    method: 'GET',
-  }).done(function(result) {
-    console.log(result);
-  }).fail(function(err) {
-    throw err;
-  });
+    });
+    
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(data) {
+      $.each(data, function(key, value) {
+
+//insert code here
+
+      });
+    }).fail(function(err) {
+      throw err;
+    });
   
   });
 
