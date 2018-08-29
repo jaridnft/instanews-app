64e15e5849675194c71cef90146238bc4277200e
jaridwarren <jaridwarren@gmail.com>
2018-08-29 16:17:17 -0700

    formatted files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    browserSync = require('browser-sync').create(),
-    eslint = require('gulp-eslint'),
-    sass = require('gulp-sass'),
-    babel = require('gulp-babel'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    prettyError = require("gulp-prettyerror");
+var gulp = require("gulp"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  browserSync = require("browser-sync").create(),
+  eslint = require("gulp-eslint"),
+  sass = require("gulp-sass"),
+  babel = require("gulp-babel"),
+  autoprefixer = require("gulp-autoprefixer"),
+  cssnano = require("gulp-cssnano"),
+  prettyError = require("gulp-prettyerror");
 
 gulp.task("sass", () => {
   return gulp
@@ -26,45 +26,51 @@ gulp.task("sass", () => {
 });
 
 gulp.task("lint", () => {
-    return gulp
-            .src('src/js/*.js')
-                // eslint() attaches the lint output to the "eslint" property
-                // of the file object so it can be used by other modules.
-            .pipe(eslint())
-                // eslint.format() outputs the lint results to the console.
-                // Alternatively use eslint.formatEach() (see Docs).
-            .pipe(eslint.format())
-                // To have the process exit with an error code (1) on
-                // lint error, return the stream and pipe to failAfterError last.
-            .pipe(eslint.failAfterError());
+  return (
+    gulp
+      .src("src/js/*.js")
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError())
+  );
 });
 
-gulp.task('scripts', gulp.series("lint", () => {
+gulp.task(
+  "scripts",
+  gulp.series("lint", () => {
     return gulp
       .src("./src/js/*.js") // these are the files gulp will consume
-      .pipe( babel())   // transcompile ES6 to ES5
-      .pipe( uglify() ) // call uglify function on these files
-      .pipe( rename ({
+      .pipe(babel()) // transcompile ES6 to ES5
+      .pipe(uglify()) // call uglify function on these files
+      .pipe(
+        rename({
           extname: ".min.js"
         })
       ) // change file extension after uglified
-      .pipe( gulp.dest("./build/js") ); // send built files to ./build/js/
-}));
+      .pipe(gulp.dest("./build/js")); // send built files to ./build/js/
+  })
+);
 
 gulp.task("watch", () => {
-    // pass in files that need to be uglified
-    gulp.watch("src/js/*.js", gulp.series("scripts"));
-    gulp.watch("src/sass/**/*.scss",gulp.series("sass"));
+  // pass in files that need to be uglified
+  gulp.watch("src/js/*.js", gulp.series("scripts"));
+  gulp.watch("src/sass/**/*.scss", gulp.series("sass"));
 });
 
 gulp.task("browser-sync", () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    // pass in files you want to refresh on
-    gulp.watch(['./build/**/*', '*.html']).on('change', browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  // pass in files you want to refresh on
+  gulp.watch(["./build/**/*", "*.html"]).on("change", browserSync.reload);
 });
 
 gulp.task("default", gulp.series("watch"));
