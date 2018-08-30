010b2ff5b55aa2cb5eef4d7a54b2e568f43b8ed4
jaridwarren <jaridwarren@gmail.com>
2018-08-29 22:32:14 -0700

    formatted with single quotes

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
   // hide loader to start
-  $(".loader").hide();
+  $('.loader').hide();
   // execute the following code when select menu is changed
-  $("select").on("change", function() {
+  $('select').on('change', function() {
     // clear old stories
-    $(".story-cell").remove();
-    $(".story-text").remove();
+    $('.story-cell').remove();
+    $('.story-text').remove();
     // store selected value
     let selectedStory = $(this).val();
     // restore margins and don't do ajax request if default selected
-    if (selectedStory === "default") {
-      $("header").removeClass("toggle");
+    if (selectedStory === 'default') {
+      $('header').removeClass('toggle');
     } else {
-      $("header").addClass("toggle");
+      $('header').addClass('toggle');
       let url = `https://api.nytimes.com/svc/topstories/v2/${selectedStory}.json`;
-      url += `?${$.param({ "api-key": "eb429d0d01c04f9a8c944a8366666c40" })}`;
+      url += `?${$.param({ 'api-key': 'eb429d0d01c04f9a8c944a8366666c40' })}`;
       // get NYT data
       $.ajax({
         url: url,
-        method: "GET",
+        method: 'GET',
         // show loading gif only while ajax request is in process
         beforeSend: function() {
-          $(".loader").show();
+          $('.loader').show();
         },
         complete: function() {
-          $(".loader").hide();
+          $('.loader').hide();
         }
       })
         .done(data => {
@@ -32,11 +32,11 @@ $(document).ready(function() {
           let count = 0;
           $.each(data.results, (key, value) => {
             // check to see if the story has an image
-            if (typeof data.results[key].multimedia[4] !== "undefined") {
+            if (typeof data.results[key].multimedia[4] !== 'undefined') {
               // only print the first 12 stories with pictures
               count += 1;
               if (count < 13) {
-                $(".stories").append(
+                $('.stories').append(
                   `<a href="${
                     data.results[key].url
                   }" target="_blank" class="story-cell"><p class='story-text'>${
@@ -44,18 +44,18 @@ $(document).ready(function() {
                   }</p></a>`
                 );
                 // set the background image of the new story
-                $(".stories")
-                  .children(":last-child")
+                $('.stories')
+                  .children(':last-child')
                   .css(
-                    "background-image",
+                    'background-image',
                     `url("${data.results[key].multimedia[4].url}")`
                   );
               }
             }
           });
           // display abstract when you hover
-          $(".story-text").hide();
-          $(".story-cell").hover(
+          $('.story-text').hide();
+          $('.story-cell').hover(
             function() {
               $(this)
                 .children()
@@ -68,7 +68,7 @@ $(document).ready(function() {
             }
           );
         })
-        .fail(() => alert("Something went wrong, please refresh."));
+        .fail(() => alert('Something went wrong, please refresh.'));
     }
   });
 });
