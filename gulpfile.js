218747ce49b1b1d5c782c2c28eca686eaa6fedab
jaridwarren <jaridwarren@gmail.com>
2018-08-30 22:39:55 -0700

    changed to single quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-var gulp = require("gulp"),
-  uglify = require("gulp-uglify"),
-  rename = require("gulp-rename"),
-  browserSync = require("browser-sync").create(),
-  eslint = require("gulp-eslint"),
-  sass = require("gulp-sass"),
-  babel = require("gulp-babel"),
-  autoprefixer = require("gulp-autoprefixer"),
-  cssnano = require("gulp-cssnano"),
-  prettyError = require("gulp-prettyerror");
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create(),
+  eslint = require('gulp-eslint'),
+  sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  prettyError = require('gulp-prettyerror');
 
-gulp.task("sass", () => {
+gulp.task('sass', () => {
   return gulp
-    .src("./src/sass/style.scss")
+    .src('./src/sass/style.scss')
     .pipe(prettyError())
     .pipe(sass())
     .pipe(
       autoprefixer({
-        browsers: ["last 2 versions"]
+        browsers: ['last 2 versions']
       })
     )
-    .pipe(gulp.dest("./build/css"))
+    .pipe(gulp.dest('./build/css'))
     .pipe(cssnano())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("./build/css"));
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task("lint", () => {
+gulp.task('lint', () => {
   return (
     gulp
-      .src("src/js/*.js")
+      .src('src/js/*.js')
       // eslint() attaches the lint output to the "eslint" property
       // of the file object so it can be used by other modules.
       .pipe(eslint())
@@ -42,37 +42,37 @@ gulp.task("lint", () => {
 });
 
 gulp.task(
-  "scripts",
-  gulp.series("lint", () => {
+  'scripts',
+  gulp.series('lint', () => {
     return gulp
-      .src("./src/js/*.js") // these are the files gulp will consume
+      .src('./src/js/*.js') // these are the files gulp will consume
       .pipe(babel()) // transcompile ES6 to ES5
       .pipe(uglify()) // call uglify function on these files
       .pipe(
         rename({
-          extname: ".min.js"
+          extname: '.min.js'
         })
       ) // change file extension after uglified
-      .pipe(gulp.dest("./build/js")); // send built files to ./build/js/
+      .pipe(gulp.dest('./build/js')); // send built files to ./build/js/
   })
 );
 
-gulp.task("watch", () => {
+gulp.task('watch', () => {
   // pass in files that need to be uglified
-  gulp.watch("src/js/*.js", gulp.series("scripts"));
-  gulp.watch("src/sass/**/*.scss", gulp.series("sass"));
+  gulp.watch('src/js/*.js', gulp.series('scripts'));
+  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
 });
 
-gulp.task("browser-sync", () => {
+gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   });
   // pass in files you want to refresh on
-  gulp.watch(["./build/**/*", "*.html"]).on("change", browserSync.reload);
+  gulp.watch(['./build/**/*', '*.html']).on('change', browserSync.reload);
 });
 
-gulp.task("default", gulp.series("watch"));
+gulp.task('default', gulp.series('watch'));
 //to turn on browser sync as well use:
 //gulp.task("default", gulp.parallel("browser-sync", "watch"));
